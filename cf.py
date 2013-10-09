d755fa34acf624ca238809ba6f7e668682f89283
dzmtr <dzmtr@users.noreply.github.com>
2013-10-09 13:23:04 +0200

    Simplified code.

--- cf.py
+++ cf.py
@@ -9,18 +9,15 @@ import urllib2
 import os.path
 import ConfigParser
 
-CONFIG = '.cfrc'
-CONFIG_SECTION = 'config'
-
-def login(url, handle, password):
+def login(handle, password):
     data = {}
     data['action'] = 'enter'
     data['handle'] = handle
     data['password'] = password
     encoded_data = urllib.urlencode(data)
-    urllib2.urlopen(url, encoded_data)
+    urllib2.urlopen('http://codeforces.ru/enter', encoded_data)
 
-def submit(url, problem, language, solution):
+def submit(contest, problem, language, solution):
     data = {}
     data['action'] = 'submitSolutionFormSubmitted'
     data['submittedProblemIndex'] = problem
@@ -28,17 +25,18 @@ def submit(url, problem, language, solution):
     with open(solution) as f:
         data['source'] = f.read()
     encoded_data = urllib.urlencode(data)
+    url = 'http://codeforces.ru/contest/{0}/submit'.format(contest)
     response = urllib2.urlopen(url, encoded_data)
     html = response.read()
     match = re.search('data-submission-id="([0-9]*)"', html)
-    submission = match.groups(1)
-    return submission
+    return match.groups(1)
 
-def check(url, submission):
+def check(submission):
     data = {}
     data['submissionId'] = submission
     encoded_data = urllib.urlencode(data)
-    response = urllib2.urlopen(url, encoded_data)
+    response = urllib2.urlopen('http://codeforces.ru/data/submissionVerdict',
+        encoded_data)
     submission_results = json.load(response)
     return submission_results
 
@@ -48,13 +46,15 @@ def write(match, problem, case, extension):
         str2 = str1.replace('<br />', '\n')
         f.write(str2)
 
-def download(url, problem):
+def download(contest, problem):
+    url = 'http://codeforces.ru/contest/{0}/problem/{1}'.format(contest,
+        problem)
     response = urllib2.urlopen(url, problem)
     html = response.read()
     iter1 = re.finditer('<div class="input"><div class="title">.*?</div>'
-'<pre>(.*?)</pre></div>', html)
+        '<pre>(.*?)</pre></div>', html)
     iter2 = re.finditer('<div class="output"><div class="title">.*?</div>'
-'<pre>(.*?)</pre></div>', html)
+        '<pre>(.*?)</pre></div>', html)
     case = 0
     for pair in zip(iter1, iter2):
         case += 1
@@ -64,49 +64,37 @@ def download(url, problem):
 
 
 
-config_parser = ConfigParser.ConfigParser()
-config_parser.read(CONFIG)
-
-if len(sys.argv) == 2:
+if len(sys.argv) > 2:
+    print "Usage: cf.py or cf.py solution"
+elif len(sys.argv) == 1:
+    config_parser = ConfigParser.ConfigParser()
+    config_parser.read('.cfrc')
 
     handler1 = urllib2.HTTPRedirectHandler()
     handler2 = urllib2.HTTPCookieProcessor()
     opener = urllib2.build_opener(handler1, handler2)
-
     urllib2.install_opener(opener)
 
-    handle = config_parser.get(CONFIG_SECTION, 'handle')
-    password = config_parser.get(CONFIG_SECTION, 'password')
-    login_url = config_parser.get(CONFIG_SECTION, 'login_url')
-
-    login(login_url, handle, password)
+    handle = config_parser.get('cf', 'handle')
+    password = config_parser.get('cf', 'password')
+    login(handle, password)
 
     solution = sys.argv[1]
+    contest = config_parser.get('cf', 'contest')
     problem, extension = os.path.splitext(solution)
-    language = config_parser.get(CONFIG_SECTION, extension)
-    submit_url = config_parser.get(CONFIG_SECTION, 'submit_url')
+    language = config_parser.get('cf', extension)
+    submission = submit(contest, problem, language, solution)
 
-    submission = submit(submit_url, problem, language, solution)
-
-    check_url = config_parser.get(CONFIG_SECTION, 'check_url')
     while True:
-
-        submission_results = check(check_url, submission)
+        submission_results = check(submission)
         print submission_results
-
         if submission_results['waiting'] == 'false':
             break
-
         time.sleep(1)
-
 elif len(sys.argv) == 1:
-
-    download_url_base = config_parser.get(CONFIG_SECTION, 'download_url_base')
-
+    config_parser = ConfigParser.ConfigParser()
+    config_parser.read('.cfrc')
+    contest = config_parser.get('cf', 'contest')
     for problem in ['a', 'b', 'c', 'd', 'e']:
-        download_url = '{0}/{1}'.format(download_url_base, problem) 
-        download(download_url, problem)
-
-else:
-
-    print "Usage: cf.py or cf.py solution"
+        download(contest, problem)
+   
